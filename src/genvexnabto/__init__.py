c7a7c7f7a3c2c5467196ad9bf2c5744ea362d000
Robin Madsen <robin@robserob.dk>
2024-07-21 03:14:04 +0200

    Bump to version 0.1.9.1

--- src/genvexnabto/__init__.py
+++ src/genvexnabto/__init__.py
@@ -1,7 +1,7 @@
 from .genvexnabto import ( GenvexNabto, GenvexNabtoConnectionErrorType )
 from .models import ( GenvexNabtoDatapointKey, GenvexNabtoSetpointKey )
 
-__version__ = "0.1.9"
+__version__ = "0.1.9.1"
 __all__ = [
     "GenvexNabto",
     "GenvexNabtoConnectionErrorType",
