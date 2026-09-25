52e7784ad1cd9327f350e3c3e87df510025b8610
Robin Madsen <robin@robserob.dk>
2026-09-25 11:40:22 +0200

    Bump to 1.5.4 for release

--- src/genvexnabto/__init__.py
+++ src/genvexnabto/__init__.py
@@ -1,7 +1,7 @@
 from .genvexnabto import ( GenvexNabto, GenvexNabtoConnectionErrorType )
 from .models import ( GenvexNabtoDatapointKey, GenvexNabtoSetpointKey )
 
-__version__ = "1.5.3"
+__version__ = "1.5.4"
 __all__ = [
     "GenvexNabto",
     "GenvexNabtoConnectionErrorType",
